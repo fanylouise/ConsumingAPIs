88e787c1af2a16254804440d477cf87148abf5df
fanylouise <gandra.fany@gmail.com>
2022-09-27 17:42:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//EXERCÍCIOS
+
+// Utilizando a API https://viacep.com.br/ws/${CEP}/json/ crie um formulário onde o usuário pode digitar o CEP e ao submeter o formulário, exiba as informaçõee do endereço completo.
+
+
+// Utilizando a API https://blockchain.info/ticker retorne no DOM o valor de compra da bitcoin and reais atualize este valor a cada 30s;
+
+
+//Utilizando a API https://api.chucknorris.io/jokes/random, exiba uma piada aleatória toda vez que o usuário clicar em um botão escrito 'próximo';
